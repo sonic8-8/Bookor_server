e019ea72063ccc95effbe4e642662d8cbf8a0f13
이승환 <seaung13@gmail.com>
2024-08-05 15:22:20 +0900

    SpringBoot 프로젝트 추가함

---
+++ back-end/src/main/java/com/smhrd/bookor/BookorApplication.java
@@ -0,0 +1,13 @@
+package com.smhrd.bookor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookorApplication.class, args);
+    }
+
+}
---
+++ back-end/src/test/java/com/smhrd/bookor/BookorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smhrd.bookor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
